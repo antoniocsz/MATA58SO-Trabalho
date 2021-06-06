214fafbd4608f621ff3ecc13451cf7c8ea2b208b
Antonio Santana <antcesar.santana@gmail.com>
2021-06-05 22:00:51 -0300

    coments

--- src/algoritimos/escalonamento/fifo.js
+++ src/algoritimos/escalonamento/fifo.js
@@ -1,11 +1,14 @@
 export function fifo(processlist) {
-  const processArray = [...processlist]
-  const numProcess = processArray.length
-  const ordensExecucao = {}
-  let tempoAtual = 0
-  let aux = 0
+  const processArray = [...processlist] // array usando dentro da lista.
+  const numProcess = processArray.length // tamanho da lista
+  const ordensExecucao = {} // estrutura de dados auxiliar
+  let tempoAtual = 0 // variavel que ajuda a contar o tempo passado durante a excução do processo
+  let aux = 0 // variavel euxiliar para ajduar nas permutas do bubblesort
 
-  // função Bubblesort para ordenar pelo tempo de chegada do processo
+  /* 
+    função que realiza um Bubblesort para ordenar os processos pelo tempo de chegada.
+    ele faz permutas de i por i+1 e chama recursivamente até percorrer o fim do processo.
+  */
   function sort(array, n) {
     for (let i = 0; i < n; i++) {
       if (array[i].tempoChegada > array[i + 1].tempoChegada) {
@@ -17,12 +20,28 @@ export function fifo(processlist) {
     }
   }
 
+  /* 
+    função que inicializa a estrutura de dados auxiliar. 
+  */
   function listStart(array) {
+    /*
+      Isso aqui equivale a um for que percorre o array, 
+      e adiciona ao orderExecução um objeto com o id do processo, com um array vazio:
+
+      for (let i=0; i < array.length; i++) {
+        ordensExecucao[array[i].id] = []
+        {  0: [], 1: [], 2: [] }
+      }
+    
+    */
     array.map((elem) => {
       ordensExecucao[elem.id] = []
     })
   }
 
+  /* 
+    função que gera os dados que são retornados para serem usados nos graficos. 
+  */
   function generateResult() {
     let result = []
     let xOrdensExecucao = Object.values(ordensExecucao)
@@ -44,8 +63,16 @@ export function fifo(processlist) {
   sort(processArray, numProcess - 1)
   listStart(processArray)
 
-  tempoAtual = processArray[0].tempoChegada
+  /*
+    Lógica do fifo
+  */
+  tempoAtual = processArray[0].tempoChegada //
   for (let i = 0; i < numProcess; i++) {
+    /* 
+      Verifica se tempo atual é mmenor ou igual ao tempo de chegada.
+      1 - incrementa o turnaround.
+      2 - Salva
+    */
     if (tempoAtual <= processArray[i].tempoChegada) {
       processArray[i].turnaround = 0
       tempoAtual = processArray[i].tempoChegada
@@ -67,6 +94,8 @@ export function fifo(processlist) {
         status: 'em-execução',
       })
     }
+
+    // Contador do Turnaround
     for (let j = 0; j < processArray[i].tempoExecucao; j++) {
       processArray[i].turnaround++
     }
